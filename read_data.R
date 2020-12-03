045980b2f7203f7ccf618e632add54319b1f5ade
Daniel <daniel.racek@chello.at>
2020-12-03 09:22:53 +0100

    Fixed the parsing failures for movie names with "," in there

--- read_data.R
+++ read_data.R
@@ -20,8 +20,16 @@ saveRDS(file = paste0(path, "/data/ratings_df.rds"), ratings_df)
 ratings_df = readRDS(file = paste0(path, "/data/ratings_df.rds"))
 
 #get movie info
-movies_path = paste0(path, "data/movie_titles.txt")
-movies_df = read_delim(movies_path, delim =",", skip=0, col_names = c("movie_id", "release_year", "name")) 
-#--> 343 parsing failures: movie names with "," in there -.-
+movies_path = paste0(path, "/data/movie_titles.txt")
+#Need to deal with problem, that some names have multiple "," in there, which is at the same time the delim
+#So what I did was to read in multiple name columns:
+movies_df = read_delim(movies_path, delim =",", skip=0, col_names = c("movie_id", "release_year", "name", "name2", "name3", "name4", "name5")) 
+#Now combine columns:
+movies_df = movies_df %>% mutate(name = ifelse(!is.na(name2), paste0(name, ",", name2) ,name))
+movies_df = movies_df %>% mutate(name = ifelse(!is.na(name3), paste0(name, ",", name3) ,name))
+movies_df = movies_df %>% mutate(name = ifelse(!is.na(name4), paste0(name, ",", name4) ,name))
+movies_df = movies_df %>% mutate(name = ifelse(!is.na(name5), paste0(name, ",", name5) ,name))
+movies_df = movies_df %>% select(-name2, -name3, -name4, -name5)
 saveRDS(file = paste0(path, "/data/movies_df.rds"), movies_df)
+
 
