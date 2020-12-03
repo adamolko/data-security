5848d4e0cb543e9d84eeba76b50c1b145f1199f4
Daniel <daniel.racek@chello.at>
2020-12-03 14:05:44 +0100

    Fixed dumb mistake in reading in data

--- read_data.R
+++ read_data.R
@@ -1,23 +1,21 @@
 library(tidyverse)
-
+library(dplyr)
 
 path ="C:/R - Workspace/data security"
 
 #get ratings 
-ratings_path = paste0(path, "data/training_set")
+ratings_path = paste0(path, "/data/training_set")
 ratings_df = tibble()
 movie_id_counter = 1
 files = list.files(ratings_path)
 for(movie_rating_path in files){
   print(movie_id_counter)
-  temp_ratings = read_delim(paste0(ratings_path, "/", test[1]), delim =",", skip=1, col_names = c("user_id", "rating", "date")) 
+  temp_ratings = read_delim(paste0(ratings_path, "/", movie_rating_path), delim =",", skip=1, col_names = c("user_id", "rating", "date")) 
   temp_ratings = temp_ratings %>% mutate(movie_id = movie_id_counter)
   ratings_df = bind_rows(ratings_df, temp_ratings)
   movie_id_counter = movie_id_counter + 1
 }
 saveRDS(file = paste0(path, "/data/ratings_df.rds"), ratings_df)
-
-ratings_df = readRDS(file = paste0(path, "/data/ratings_df.rds"))
 
 #get movie info
 movies_path = paste0(path, "/data/movie_titles.txt")
