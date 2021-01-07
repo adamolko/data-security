fe257d7cee7aec3020346ec7f8eab6726c5abe67
Cornelia Gruber <cornelia.gruber@campus.lmu.de>
2021-01-07 17:06:03 +0100

    descriptives

--- movielens_descriptives.R
+++ movielens_descriptives.R
@@ -1,6 +1,6 @@
 library(ggplot2)
 library(readr)
-
+library(dplyr)
 
 data <- read.delim("~/R/data-security/data/ml-100k/u.data", header = FALSE)
 # user id | item id | rating | timestamp
@@ -25,13 +25,14 @@ tbl$Freq <- as.numeric(tbl$Freq)
 
 # plotting ----------------------------------------------------------------
 
-#histogram users
+#histogram users add numbers for mean and median
 ggplot(tbl_user, aes(Freq)) +
   geom_histogram(bins = 40) +
   geom_vline(aes(xintercept = mean(Freq), linetype = "mean"), col = "red") +
   geom_vline(aes(xintercept = median(Freq), linetype = "median"), col = "red") +
-  labs(title="Histogram Ratings per Movie", x ="Frequency", y = "") +
-  scale_color_manual(name = "statistics", values = c(median = "dashed", mean = "solid"))
+  labs(title = "Histogram Ratings per Movie", x = "Frequency", y = "") +
+  scale_color_manual(name = "statistics", values = c(median = "dashed", 
+                                                     mean = "solid"))
 
 
 #histogram movies
@@ -39,13 +40,14 @@ ggplot(tbl_movies, aes(Freq)) +
   geom_histogram(bins = 40) +
   geom_vline(aes(xintercept = mean(Freq), linetype = "mean"), col = "red") +
   geom_vline(aes(xintercept = median(Freq), linetype = "median"), col = "red") +
-  labs(title="Histogram Ratings per Movie", x ="Frequency", y = "") +
-  scale_color_manual(name = "statistics", values = c(median = "dashed", mean = "solid"))
+  labs(title="Histogram Ratings per Movie", x = "Frequency", y = "") +
+  scale_color_manual(name = "statistics", values = c(median = "dashed",
+                                                     mean = "solid"))
 
 
 
 #boxplot both
-ggplot(tbl, aes(Freq, group = ratings, col = ratings)) + 
+ggplot(tbl, aes(x = Freq, y = ratings, col = ratings)) + 
   geom_boxplot()
 
 
@@ -57,6 +59,8 @@ ggplot(tbl, aes(Freq, group = ratings, col = ratings)) +
 
 # !! BE CAREFUL !!
 # many moevies are in the list with 2 different ids
+
+# TODO which movies twice?
 
 train <- read.delim("~/R/data-security/data/ml-100k/u1.base", header=FALSE)
 test <- read.delim("~/R/data-security/data/ml-100k/u1.test", header=FALSE)
@@ -74,6 +78,27 @@ tbl_test_m <- table(test$item)
 tbl_test_m <- as.data.frame(tbl_test_m)
 tbl_test_m <- tbl_test_m[order(-tbl_test_m$Freq),]
 
+
+
+# analyze ratings ---------------------------------------------------------
+mean_rating <- data %>% group_by(item) %>% 
+  summarise(avg_rating = mean(rating), n = n(), sd = sd(rating))
+
+
+ggplot(mean_rating, aes(avg_rating))+
+  geom_histogram()
+
+ggplot(mean_rating, aes(avg_rating))+
+  geom_boxplot()
+
+
+ggplot(mean_rating, aes(n))+
+  geom_histogram()
+
+
+# analyze movies ----------------------------------------------------------
+
+
 item <- read_delim("data/ml-100k/u.item", "|", escape_double = FALSE,
                    col_names = FALSE, trim_ws = TRUE)
 colnames(item) <- c("movie_id", "movie_title", "release_date", 
@@ -84,10 +109,9 @@ colnames(item) <- c("movie_id", "movie_title", "release_date",
                     "War", "Western")
 
 
-
-item$release_date <- as.POSIXct(item$release_date, format = "%d-%b-%Y")
-
-
+#make sure date abbreviation is english
+Sys.setlocale("LC_TIME", "English")
+item$release_date <- as.Date(item$release_date, format = "%d-%h-%Y")
 
 # 50, 100, 286 ,1 check some much rated movies
 item[c(1,50, 56, 100, 286),1:2]
@@ -128,18 +152,103 @@ tbl_test_u <- tbl_test_u[order(-tbl_test_u$Freq),]
 mib <- data[data$item == 257, ]
 no_mib <- unique(data[!(data$user %in% mib$user),"user"])
 no_mib <- sort(no_mib)
-write.table(no_mib, "data/ml-100k/users_no_mib.txt", row.names = FALSE, col.names = FALSE)
+write.table(no_mib, "data/ml-100k/users_no_mib.txt", row.names = FALSE,
+            col.names = FALSE)
 
 
 toy <- data[data$item == 1, ]
 no_toy <- unique(data[!(data$user %in% toy$user),"user"])
 no_toy <- sort(no_toy)
-write.table(no_toy, "data/ml-100k/users_no_toy.txt", row.names = FALSE, col.names = FALSE)
+write.table(no_toy, "data/ml-100k/users_no_toy.txt", row.names = FALSE,
+            col.names = FALSE)
 
 
 no_both <- no_mib[no_mib %in% no_toy]
-write.table(no_both, "data/ml-100k/users_no_both.txt", row.names = FALSE, col.names = FALSE)
+write.table(no_both, "data/ml-100k/users_no_both.txt", row.names = FALSE,
+            col.names = FALSE)
 
 
 tbl_train_u[no_both %in% tbl_train_u$Var1,]
+
+
+
+
+# descriptives movies -----------------------------------------------------
+
+genres <- colnames(item[,6:24])
+
+tab_genres <- as.data.frame(colSums(item[,genres]))
+
+tab_genres$genre <- as.factor(rownames(tab_genres))
+colnames(tab_genres)[1] <- "Freq"
+
+ggplot(tab_genres, aes(x = reorder(genre, -Freq), y = Freq)) +
+  geom_col()+
+  geom_text(label = tab_genres$Freq, vjust=-0.4)+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+
+
+ggplot(item, aes(x = format(release_date, "%Y")))+
+  geom_bar()+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+count.or <- function(dat, n = 2) {
+  or.sum <- function(cols) sum(rowSums(dat[cols]) > 0)
+  counts <- combn(colnames(dat), n, FUN = or.sum)
+  names  <- combn(colnames(dat), n, FUN = paste, collapse = "&")
+  setNames(counts, names)
+}
+
+
+count.or(item[,genres], 2)
+
+
+
+crosstab_genres <- item[,genres] %>% #dplyr::select(-movie_title) %>% 
+  dplyr::mutate_all(function(x) ifelse(x<1, NA, x)) %>%
+  psych::pairwiseCount()
+
+
+heatmap(crosstab_genres)
+
+
+
+
+crosstab_genres[lower.tri(crosstab_genres)] <- NA
+
+diag(crosstab_genres)<- NA
+library(reshape2)
+melted_cormat <- melt(crosstab_genres)
+head(melted_cormat)
+
+
+ggplot(data = melted_cormat, aes(x=Var1, y=Var2, fill=value)) + 
+  geom_tile()
+
+
+
+# user --------------------------------------------------------------------
+user <- read_delim("data/ml-100k/u.user", "|", escape_double = FALSE,
+                   col_names = FALSE, trim_ws = TRUE)
+colnames(user) <- c("id", "age", "gender", "occupation", "zip_code") 
+
+ggplot(user, aes(age)) +
+  geom_bar()
+
+ggplot(user, aes(age,y = gender, fill = gender)) +
+  geom_boxplot()
+
+
+ggplot(user, aes(reorder(occupation,occupation, function(x)-length(x)))) +
+  geom_bar()+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+mean_rating[mean_rating$item == 1,]
+
+
+item[item$movie_id == 12,]
+
+
+test[test$item == 1,]
 
